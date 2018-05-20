45e765951d354e09adc3f2384cf74a2b78897952
Norton Allen <allen@huarp.harvard.edu>
2018-05-19 20:46:45 -0400

    Converted to CPP Project

--- uDACS_V1_1_1/control.h
+++ uDACS_V1_1_1/control.h
@@ -2,10 +2,18 @@
 #define CONTROL_H_INCLUDED
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void SendMsg(const char *);			// Send String Back to Host via USB
 void SendCode(int8_t code);
 void SendCodeVal(int8_t, uint16_t);
 void SendErrorMsg(const char *msg);
 void poll_control(void);
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif
